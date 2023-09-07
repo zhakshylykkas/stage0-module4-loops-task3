4b9428ad97c6951823254d1b909a073aba66e560
Zhakshylyk Kasymbekov <jakkasymbekov@gmail.com>
2023-09-07 16:57:38 +0600

    Changes committed to DigitsSum and GreatestCommonDivisor tasks after the second testing

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,15 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public static void main(String[] args) {
-        printDigitsSum(-1239);
+        printDigitsSum(-499);
     }
     public static void printDigitsSum(int t){
         String intToStr = String.valueOf(t);
         int digitsSum = 0;
         int startPoint = 0;
         if (intToStr.startsWith("-")) {
-            digitsSum = Integer.parseInt(String.valueOf(intToStr.charAt(1))) * (-1);
-            startPoint = 2;
+            startPoint = 1;
         }
         for (int i = startPoint; i < intToStr.length(); i++) {
             digitsSum += Integer.parseInt(String.valueOf(intToStr.charAt(i)));
--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,15 +2,19 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public static void main(String[] args) {
-        printGCD(6, 12);
+        printGCD(0, 30);
     }
     public static void printGCD(int first, int second) {
         int greatestCmnDvsrSoFar = 0;
-        for (int i = 1; first < second ? i <= first : i <= second; i++) {
-            if (first % i == 0 && second % i == 0) {
-                greatestCmnDvsrSoFar = i;
+        if (first == 0 || second == 0) {
+            System.out.println(Math.max(first, second));
+        } else {
+            for (int i = 1; first < second ? i <= first : i <= second; i++) {
+                if (first % i == 0 && second % i == 0) {
+                    greatestCmnDvsrSoFar = i;
+                }
             }
+            System.out.println(greatestCmnDvsrSoFar);
         }
-        System.out.println(greatestCmnDvsrSoFar);
     }
 }
